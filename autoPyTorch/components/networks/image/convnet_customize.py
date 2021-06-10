124256668fddd02b66aa05058d206dbee2f1ce7c
lindsey98 <liny@xgpd5.comp.nus.edu.sg>
2021-06-10 20:17:48 +0800

    update code

--- autoPyTorch/components/networks/image/convnet_customize.py
+++ autoPyTorch/components/networks/image/convnet_customize.py
@@ -36,7 +36,6 @@ class ConvCusNet(BaseImageNet):
         init_filter = self.config["conv_init_filters"]
         second_filter = self.config["conv_second_filters"]
         third_filter = self.config["conv_third_filters"]
-        dense_filter = self.config["fc_first_size"]
         
         # 3 CNN layers
         self._add_layer(layers, self.channels, init_filter, 1)
@@ -54,8 +53,8 @@ class ConvCusNet(BaseImageNet):
             
         # 2 dense layers
         dense_layers = list()
-        self._add_dense(dense_layers, self.dense_size, dense_filter, 4, True)
-        self._add_dense(dense_layers, dense_filter, out_features, 5, False) # last layer shall have no activation
+        self._add_dense(dense_layers, self.dense_size, 32, True)
+        self._add_dense(dense_layers, 32, out_features, False) # last layer shall have no activation
 
         nw = nn.Sequential(*layers)
         nd = nn.Sequential(*dense_layers)
@@ -63,47 +62,35 @@ class ConvCusNet(BaseImageNet):
         return nw, nd
     
     def _get_layer_size(self, w, h):
-        cw = ((w - self.config["conv_kernel_size"] + 2 * self.config["conv_kernel_padding"])
-                //self.config["conv_kernel_stride"]) + 1
-        ch = ((h - self.config["conv_kernel_size"] + 2 * self.config["conv_kernel_padding"])
-                //self.config["conv_kernel_stride"]) + 1
-        cw, ch = cw // self.config["pool_size"], ch // self.config["pool_size"]
+        cw = w
+        ch = h
+        cw, ch = cw // 2, ch // 2
         return cw, ch
 
     def _add_layer(self, layers, in_filters, out_filters, layer_id):
         layers.append(nn.Conv2d(in_filters, out_filters,
-                                kernel_size=self.config["conv_kernel_size"],
-                                stride=self.config["conv_kernel_stride"],
-                                padding=self.config["conv_kernel_padding"]))
+                                kernel_size=3,
+                                stride=1,
+                                padding=1))
         layers.append(nn.BatchNorm2d(out_filters))
-        layers.append(self.activation())
-        layers.append(nn.MaxPool2d(kernel_size=self.config["pool_size"], stride=self.config["pool_size"]))
-    
-    def _add_dense(self, layers, in_filters, out_filters, layer_id, activate=True):
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.MaxPool2d(kernel_size=2))
+
+        
+    def _add_dense(self, layers, in_filters, out_filters, activate=True):
         layers.append(nn.Conv2d(in_filters, out_filters, # write FC layer in Convolutional way
                                 kernel_size=1, 
                                 bias=True))
         if activate:
-            layers.append(self.activation())    
+            layers.append(nn.ReLU(inplace=True))    
         
     @staticmethod
     def get_config_space(user_updates=None):
         cs = CS.ConfigurationSpace()
         
-        cs.add_hyperparameter(CSH.CategoricalHyperparameter('activation', ['relu'])) #'sigmoid', 'tanh',
-        num_layers = CSH.UniformIntegerHyperparameter('num_layers', lower=3, upper=3) # FIXME
-        cs.add_hyperparameter(num_layers)
-        
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('conv_init_filters', lower=8, upper=64))
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('conv_second_filters', lower=8, upper=64))
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('conv_third_filters', lower=8, upper=64))
-        
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('conv_kernel_size', lower=3, upper=3))
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('conv_kernel_stride', lower=1, upper=1))
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('conv_kernel_padding', lower=1, upper=1))
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('pool_size', lower=2, upper=2)) # FIXME: I limit the upper to be same as lower
-        
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter('fc_first_size', lower=32, upper=32))
 
         return(cs)
 # 
