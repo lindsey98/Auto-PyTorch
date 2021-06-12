365173ed7956b7c71d4879a2ba5ffea74a584ec1
lindsey98 <liny@xgpd5.comp.nus.edu.sg>
2021-06-12 15:57:18 +0800

    minor

--- examples/basics/tutorial.py
+++ examples/basics/tutorial.py
@@ -1,41 +1,4 @@
-#%%
-
-# import unittest
-# import torch
-# import torch.nn as nn
-#
-# from autoPyTorch.pipeline.base.pipeline import Pipeline
-# from autoPyTorch.components.networks.image import ConvCusNet
-# from autoPyTorch.pipeline.nodes.image.network_selector_datasetinfo import NetworkSelectorDatasetInfo
-# from autoPyTorch.pipeline.nodes.image.create_dataset_info import DataSetInfo
-#
-# #%%
-#
-# pipeline = Pipeline([
-#     NetworkSelectorDatasetInfo()
-# ])
-# dataset_info = DataSetInfo()
-# selector = pipeline[NetworkSelectorDatasetInfo.get_name()]
-# selector.add_network("convnet_customize", ConvCusNet)
-# selector.add_final_activation('none', nn.Sequential())
-#
-# pipeline_config = pipeline.get_pipeline_config()
-# pipeline_config["random_seed"] = 42
-# hyper_config = pipeline.get_hyperparameter_search_space().sample_configuration()
-
-# pipeline.fit_pipeline(hyperparameter_config=hyper_config, pipeline_config=pipeline_config,
-#                         X=torch.rand(1,3,32,32), Y=torch.rand(1, 1))
-
-#%% md
-
-# Image classification
-
-
-#%%
-
-from autoPyTorch import AutoNetImageClassification, AutoNetClassification
-
-#%%
+from autoPyTorch import AutoNetImageClassification
 
 # Other imports for later usage
 import pandas as pd
@@ -44,57 +7,21 @@ import os as os
 import openml
 import json
 
-#%%
-
-from autoPyTorch.utils.hyperparameter_search_space_update import HyperparameterSearchSpaceUpdates
-
-
-def get_search_space_updates():
-    """
-    Search space updates to the task can be added using HyperparameterSearchSpaceUpdates
-    Returns:
-        HyperparameterSearchSpaceUpdates
-    """
-    updates = HyperparameterSearchSpaceUpdates()
-    updates.append(node_name="NetworkSelector",
-                                hyperparameter="convnet_customize:activation",
-                                value_range=["relu"])
-
-    updates.append(node_name="NetworkSelector",
-                                hyperparameter="convnet_customize:conv_init_filters",
-                                value_range=[8, 64],
-                                log=True)
-    updates.append(node_name="NetworkSelector",
-                                hyperparameter="convnet_customize:conv_second_filters",
-                                value_range=[8, 64],
-                                log=True)
-
-    updates.append(node_name="NetworkSelector",
-                                hyperparameter="convnet_customize:conv_third_filters",
-                                value_range=[8, 64],
-                                log=True)
-    return updates
-
-#%% md
-
-#%%
 
 autonet_config = {
     "budget_type" : "epochs",
     "images_shape": [3,32,32],
-    # "networks": ["convnet_cus", "resnet"]
     "networks": ["convnet_cus"],
-    # "loss_modules": ["cross_entropy"],
+    "loss_modules": ["cross_entropy"],
     "batch_loss_computation_techniques": ["standard"],
-    # "lr_scheduler": ["step"],
-    # "optimizer": ["sgd"],
+    "lr_scheduler": ["step"],
+    "optimizer": ["sgd"],
     }
 
 
 autonet = AutoNetImageClassification(config_preset="full_cs",
                                      result_logger_dir="logs/",
-                                     # hyperparameter_search_space_updates=get_search_space_updates(),
-                                     **autonet_config
+                                     **autonet_config,
                                     )
 
 
@@ -107,6 +34,8 @@ hyperparameter_search_space = autonet.get_hyperparameter_search_space()
 # Print all possible configuration options
 autonet.print_help()
 
+#%%
+
 csv_dir = os.path.abspath("./datasets/cifar-10/train.csv")
 df = pd.read_csv(csv_dir, header=None)
 X_train = df.values[:,0]
@@ -114,23 +43,15 @@ Y_train = df.values[:,1]
 print(X_train.shape)
 print(Y_train.shape)
 
-results_fit = autonet.fit(X_train=X_train,
+results_fit = autonet.fit(
+                         log_level='debug',
+                         X_train=X_train,
                          Y_train=Y_train,
                          images_shape=[3,32,32],
-                         min_budget=10,
-                         max_budget=20,
+                         min_budget=1,
+                         max_budget=5,
                          max_runtime=1800,
                         images_root_folders=[os.path.abspath("./datasets/cifar-10/train/train")],
                         )
 
-
-# See how the random configuration performs (often it just predicts 0)
-# score = autonet.score(X_test=X_test, Y_test=Y_test)
-# pred = autonet.predict(X=X_test)
-
-# print("Model prediction:", pred[0:10])
-# print("Accuracy score", score)
-
-
-pytorch_model = autonet.get_pytorch_model()
-print(pytorch_model)
+#%%
